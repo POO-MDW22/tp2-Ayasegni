e45078283903d058c9e7e07bf969b6a3252b9f4a
Ayasegni <147283272+Ayasegni@users.noreply.github.com>
2023-11-24 09:44:40 +0100

    Add files via upload

--- src/tp4/Magasin.java
+++ src/tp4/Magasin.java
@@ -1,24 +1,45 @@
-import javax.xml.crypto.Data;
-public class magasin {
+public class Magasin {
+
     String adress;
     int id;
     int Capasitee;
-    produit[] products;
-    magasin(String adress,int id,int capasitee,produit[] p){
+    static int nbr;
+    Produit[] products;
+    Employe[]employes;
+    String nom;
+    Magasin (){}
+
+    Magasin(String adress,int id,int capasitee,Produit[] p, Employe[] e,String nom){
         this.adress=adress;
         this.id=id;
         this.Capasitee=capasitee;
-
+        this.products = new Produit[50];
         this.products=p;
+        this. employes= new  Employe[20];
+        this. employes=e;
+        this.nom=nom;
+        nbr ++;
     }
-    void Affichermagasin(){
-        System.out.println("id of the Market: "+id + " located in "+adress +" that supports "+Capasitee+" product");
+
+
+    public boolean ajouterEmploye(Employe e) {
+        if (nbr <20) {
+            employes[nbr]=e;
+            nbr++;}
+        return false;
+    }
+
+    void AfficherMagasin(){
+        System.out.println(id +" "+ adress +" "+Capasitee);
         for (int i=0;i<products.length;i++){
             if (products[i] != null) {
                 products[i].afficherProduit();
-            }
-        }
+            }}
     }
-}
-    
+    void AfficherMagasinemployee(){
+        for (int j=0;j<employes.length;j++) {
+            if (employes[j] != null) {
 
+                employes[j].AfficherEmploye();
+
+            }}}}
